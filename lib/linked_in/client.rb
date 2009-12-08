e5bb86437ec67c115eee85ce6bf0b3b513946cee
Nicolas Fouché <nicolas@silentale.com>
2009-12-08 22:31:57 +0800

    Fixed the error handling. Even if the XML answer contains the HTTP status code, LinkedIn also sets this code in the HTTP answer (thankfully). Lazily inspired from the 'twitter' gem.

--- lib/linked_in/client.rb
+++ lib/linked_in/client.rb
@@ -145,14 +145,24 @@ module LinkedIn
       end
       
       def raise_errors(response)
+        # Even if the XML answer contains the HTTP status code, LinkedIn also sets this code
+        # in the HTTP answer (thankfully).
         case response.code.to_i
+          when 400
+            data = LinkedIn::Error.from_xml(response.body)
+            raise RateLimitExceeded.new(data), "(#{response.code}): #{response.message} - #{data.code if data}"
+          when 401
+            data = LinkedIn::Error.from_xml(response.body)
+            raise Unauthorized.new(data), "(#{response.code}): #{response.message} - #{data.code if data}"
+          when 403
+            data = LinkedIn::Error.from_xml(response.body)
+            raise General.new(data), "(#{response.code}): #{response.message} - #{data.code if data}"
+          when 404
+            raise NotFound, "(#{response.code}): #{response.message}"
+          when 500
+            raise InformLinkedIn, "LinkedIn had an internal error. Please let them know in the forum. (#{response.code}): #{response.message}"
           when 502..503
             raise Unavailable, "(#{response.code}): #{response.message}"
-        end
-        
-        if response.body && response.body.include?("<error>")
-          error = LinkedIn::Error.from_xml(response.body)
-          Raise LinkedInError, "(#{error.status}): #{error.code} - #{error.message}"
         end
       end
       
