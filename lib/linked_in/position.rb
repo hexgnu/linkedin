a8169b179d197b33e7c509763302b3b6866c3d9b
Zach Holman <github.com@zachholman.com>
2010-01-14 05:34:50 +0800

    Expands Position to return end-date fields

--- lib/linked_in/position.rb
+++ lib/linked_in/position.rb
@@ -7,6 +7,9 @@ module LinkedIn
     xml_reader :summary
     xml_reader :start_year, :from => "start-date/year"
     xml_reader :start_month, :from => "start-date/month"
+    xml_reader :end_year, :from => "end-date/year"
+    xml_reader :end_month, :from => "end-date/month"
+    xml_reader :is_current
     xml_reader :company, :as => Company
   end
 end
