b1fd67db07ed425807dee4745dd99d114359e021
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-02-05 14:33:47 +0100

    linkedin module spec

---
+++ spec/cases/linkedin_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe LinkedIn do
+  
+  before(:each) do
+    LinkedIn.token = ''
+    LinkedIn.secret = ''    
+  end
+  
+  it "should be able to set the consumer token and consumer secret" do
+    LinkedIn.token = 'consumer_token'
+    LinkedIn.secret = 'consumer_secret'
+
+    LinkedIn.token.should == 'consumer_token'
+    LinkedIn.secret.should == 'consumer_secret'
+  end
+  
+  it "should be able to set the default profile fields" do
+    LinkedIn.default_profile_fields = ['education', 'positions']
+
+    LinkedIn.default_profile_fields.should == ['education', 'positions']
+  end
+  
+  it "should be able to set the consumer token and consumer secret via a configure block" do
+    LinkedIn.configure do |config|
+      config.token = 'consumer_token'
+      config.secret = 'consumer_secret'
+      config.default_profile_fields = ['education', 'positions']
+    end
+    
+    LinkedIn.token.should == 'consumer_token'
+    LinkedIn.secret.should == 'consumer_secret'
+    LinkedIn.default_profile_fields.should == ['education', 'positions']
+  end
+  
+end
