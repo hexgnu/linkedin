159d5131134a753b6b782b95336b48961d7e4ffc
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-02-05 18:18:07 +0100

    corrections to the recommendations patch

--- lib/linked_in/recommendations.rb
+++ lib/linked_in/recommendations.rb
@@ -1,13 +1,42 @@
 module LinkedIn
+
   class Recommendations < LinkedIn::Base
+
     def recommendations
-      @array ||= begin
-        @array = []
-        @doc.children.each do |recommendation|
-          @array << Recommendation.new(recommendation) unless recommendation.blank?
-        end
-        @array
+      @recommendations ||= @doc.children.inject([]) do |list, recommendation|
+        list << Recommendation.new(recommendation) unless recommendation.blank?
+        list
       end
     end
+
+    class Recommendation
+      def initialize(recommendation)
+        @recommendation = recommendation
+      end
+
+      %w[id recommendation_type recommendation_text].each do |f|
+        define_method(f.to_sym) do
+          @recommendation.xpath("./#{f.gsub(/_/,'-')}").text
+        end
+      end
+
+      def recommender
+        @recommender ||= Recommender.new(@recommendation.xpath('./recommender'))
+      end
+    end
+
+    class Recommender
+      def initialize(recommender)
+        @recommender = recommender
+      end
+
+      %w[id first_name last_name].each do |f|
+        define_method(f.to_sym) do
+          @recommender.xpath("./#{f.gsub(/_/,'-')}").text
+        end
+      end
+    end
+
   end
+
 end
