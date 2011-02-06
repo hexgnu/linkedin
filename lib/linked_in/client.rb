40fb8b95c6ceddbd28a5cae81d1ba2f8336c0830
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-02-06 11:18:53 +0100

    moved oauth authorization methods to their own module

--- lib/linked_in/client.rb
+++ lib/linked_in/client.rb
@@ -2,6 +2,7 @@ module LinkedIn
   class Client
     include ToXmlHelpers
     include RequestHelpers
+    include AuthorizationHelpers
 
     # TODO: @ http://developer.linkedin.com/docs/DOC-1061 && / DOC-1014
     # add in client.get("/people/~:(im-accounts)")
@@ -17,41 +18,6 @@ module LinkedIn
         :authorize_path     => "/uas/oauth/authorize"
       }
       @ctoken, @csecret, @consumer_options = ctoken, csecret, opts.merge(options)
-    end
-
-    def consumer
-      @consumer ||= begin
-        options = { :site => 'https://api.linkedin.com' }.merge(@consumer_options)
-        ::OAuth::Consumer.new(@ctoken, @csecret, options)
-      end
-    end
-
-    def set_callback_url(url)
-      clear_request_token
-      request_token(:oauth_callback => url)
-    end
-
-    # Note: If using oauth with a web app, be sure to provide :oauth_callback.
-    # Options:
-    #   :oauth_callback => String, url that LinkedIn should redirect to
-    def request_token(options={})
-      @request_token ||= consumer.get_request_token(options)
-    end
-
-    # For web apps use params[:oauth_verifier], for desktop apps,
-    # use the verifier is the pin that LinkedIn gives users.
-    def authorize_from_request(rtoken, rsecret, verifier_or_pin)
-      request_token = ::OAuth::RequestToken.new(consumer, rtoken, rsecret)
-      access_token = request_token.get_access_token(:oauth_verifier => verifier_or_pin)
-      @atoken, @asecret = access_token.token, access_token.secret
-    end
-
-    def access_token
-      @access_token ||= ::OAuth::AccessToken.new(consumer, @atoken, @asecret)
-    end
-
-    def authorize_from_access(atoken, asecret)
-      @atoken, @asecret = atoken, asecret
     end
 
 
@@ -160,10 +126,6 @@ module LinkedIn
     end
 
     private
-
-      def clear_request_token
-        @request_token = nil
-      end
 
       def format_options_for_query(opts)
         opts.keys.each do |key|
