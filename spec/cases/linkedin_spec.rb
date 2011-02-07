83a89fe99899bac07d948fe97543d42a21066980
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-02-07 17:24:21 +0100

    tiny tiny alignment change

--- spec/cases/linkedin_spec.rb
+++ spec/cases/linkedin_spec.rb
@@ -3,15 +3,15 @@ require 'spec_helper'
 describe LinkedIn do
   
   before(:each) do
-    LinkedIn.token = ''
-    LinkedIn.secret = ''    
+    LinkedIn.token = nil
+    LinkedIn.secret = nil    
   end
   
   it "should be able to set the consumer token and consumer secret" do
-    LinkedIn.token = 'consumer_token'
+    LinkedIn.token  = 'consumer_token'
     LinkedIn.secret = 'consumer_secret'
 
-    LinkedIn.token.should == 'consumer_token'
+    LinkedIn.token.should  == 'consumer_token'
     LinkedIn.secret.should == 'consumer_secret'
   end
   
@@ -23,12 +23,12 @@ describe LinkedIn do
   
   it "should be able to set the consumer token and consumer secret via a configure block" do
     LinkedIn.configure do |config|
-      config.token = 'consumer_token'
+      config.token  = 'consumer_token'
       config.secret = 'consumer_secret'
       config.default_profile_fields = ['education', 'positions']
     end
     
-    LinkedIn.token.should == 'consumer_token'
+    LinkedIn.token.should  == 'consumer_token'
     LinkedIn.secret.should == 'consumer_secret'
     LinkedIn.default_profile_fields.should == ['education', 'positions']
   end
