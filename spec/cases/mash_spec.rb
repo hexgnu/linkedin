c15f747a5f5baf8042e9e9f0a9c7804d2a320577
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-02-07 14:52:43 +0100

    some Mash spec cleanup

--- spec/cases/mash_spec.rb
+++ spec/cases/mash_spec.rb
@@ -48,52 +48,36 @@ describe LinkedIn::Mash do
   
   describe '#timestamp' do
     it "should return a valid Time if a key of timestamp exists and the value is an int" do
-      timestamp = Time.now
-    
-      time = {
-        'timestamp' => timestamp.to_i
-      }
-    
-      time_mash = LinkedIn::Mash.new(time)
-    
-      time_mash.timestamp.class.should be Time
-      timestamp.to_i.should == time_mash.timestamp.to_i
+      time_mash = LinkedIn::Mash.new({ 'timestamp' => 1297083249 })
+      
+      time_mash.timestamp.should be_a_kind_of(Time)
+      time_mash.timestamp.to_i.should  == 1297083249
     end
 
     it "should return a valid Time if a key of timestamp exists and the value is an int which is greater than 9999999999" do
-      timestamp = Time.now
-    
-      time = {
-        'timestamp' => timestamp.to_i * 1000
-      }
-    
-      time_mash = LinkedIn::Mash.new(time)
-    
-      time_mash.timestamp.class.should be Time
-      timestamp.to_i.should == time_mash.timestamp.to_i
+      time_mash = LinkedIn::Mash.new({ 'timestamp' => 1297083249 * 1000 })
+      
+      time_mash.timestamp.should be_a_kind_of(Time)
+      time_mash.timestamp.to_i.should  == 1297083249
     end
     
     it "should not try to convert to a Time object if the value isn't an Integer" do
-      time = {
-        'timestamp' => 'Foo'
-      }
-    
-      time_mash = LinkedIn::Mash.new(time)
-    
+      time_mash = LinkedIn::Mash.new({ 'timestamp' => 'Foo' })
+      
       time_mash.timestamp.class.should be String
     end
   end
   
   describe "#to_date" do
-    it "should return a valid Date if the keys year, month, day all exist" do
-      date = {
+    let(:date_mash) do
+      LinkedIn::Mash.new({
         'year' => 2010,
         'month' => 06,
         'day' => 23
-      }
+      })
+    end
     
-      date_mash = LinkedIn::Mash.new(date)
-    
+    it "should return a valid Date if the keys year, month, day all exist" do
       date_mash.to_date.should == Date.civil(2010, 06, 23)
     end
   end
