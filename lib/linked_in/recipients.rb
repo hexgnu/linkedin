ce14db960c5d2d4db1cc5785dc2b9e92dee2ff66
Leonid Shevtsov <leonid@shevtsov.me>
2011-02-17 19:32:05 +0200

    proper XML generation with Nokogiri

--- lib/linked_in/recipients.rb
+++ lib/linked_in/recipients.rb
@@ -4,9 +4,14 @@ module LinkedIn
     attr_accessor :recipients
 
     def to_xml
-      recipients.inject('') do |result, recipient|
-        result << %Q{<recipient><person path="#{recipient.person.path}"/></recipient>}
-        result
+      self.to_xml_nodes(Nokogiri.XML('<root/>', nil, 'UTF-8')).to_xml
+    end
+    
+    def to_xml_nodes(doc)
+      recipients.inject(Nokogiri::XML::NodeSet.new(doc)) do |nodes, recipient|
+        node = Nokogiri::XML::DocumentFragment.new(doc, '<recipient><person/></recipient>')
+        node.at_css('person')['path'] = recipient.person.path
+        nodes << node
       end
     end
   end
