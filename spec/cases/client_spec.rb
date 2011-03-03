9089891db94d14795626e258a4568763fd5e5804
Ben Woosley <ben.woosley@gmail.com>
2011-03-03 15:44:04 -0800

    Orgainze the rest of the top-level client specs with describe & context

--- spec/cases/client_spec.rb
+++ spec/cases/client_spec.rb
@@ -77,6 +77,41 @@ describe LinkedIn::Client do
           client.profile.site_standard_profile_request.should == "http://www.linkedin.com/profile?viewProfile=&key=3559698&authToken=yib-&authType=name"
         end
 
+        it "should retrieve skills from a profile" do
+          skill = client.profile.skills.last
+          skill.name.should == "Union negotiations"
+          skill.id.to_i.should == 38
+        end
+
+        it "should retrieve phone_number from a profile" do
+          pn = client.profile.phone_numbers.last
+
+          pn.phone_type.should == "mobile"
+          pn.phone_number.should == "+444444444444"
+        end
+
+        it "should retrieve publications from a profile" do
+          publication = client.profile.publications.last
+
+          publication.id.to_i.should == 31
+          publication.title.should == "How to host an awesome podcast"
+          publication.date.should == Date.civil(y=2006,m=8,d=1)
+        end
+
+        it "should retrieve patents from a profile" do
+          patent = client.profile.patents.last
+
+          patent.id.to_i.should == 51
+          patent.title.should == "Time machine"
+          patent.date.should == Date.civil(y=2008,m=7,d=23)
+        end
+
+        it "should retrieve language information from a profile" do
+          language = client.profile.languages.last
+          language.name.should == "Klingon"
+          language.id.to_i.should == 72
+        end
+
         context "with field selection" do
           it "should accept field selectors when retrieving a profile" do
             stub_get("/v1/people/~:(first-name,last-name)", "profile.xml")
@@ -155,136 +190,111 @@ describe LinkedIn::Client do
       end
     end
 
-    it "should update a user's current status" do
-      stub_put("/v1/people/~/current-status", "blank.xml")
+    describe "#update_status" do
+      it "should update a user's current status" do
+        stub_put("/v1/people/~/current-status", "blank.xml")
 
-      client.update_status("Testing out the LinkedIn API").code.should == "200"
+        client.update_status("Testing out the LinkedIn API").code.should == "200"
+      end
     end
 
-    it "should post to a user's network stream" do
-      stub_post("/v1/people/~/person-activities", "blank.xml")
+    describe "#update_network" do
+      it "should post to a user's network stream" do
+        stub_post("/v1/people/~/person-activities", "blank.xml")
 
-      client.update_network("Testing out the LinkedIn API").code.should == "201"
+        client.update_network("Testing out the LinkedIn API").code.should == "201"
+      end
     end
 
-    it "should clear a user's current status" do
-      stub_delete("/v1/people/~/current-status", "blank.xml")
+    describe "#clear_status" do
+      it "should clear a user's current status" do
+        stub_delete("/v1/people/~/current-status", "blank.xml")
 
-      client.clear_status.should == "200"
+        client.clear_status.should == "200"
+      end
     end
 
-    it "should retrieve the authenticated user's current status" do
-      stub_get("/v1/people/~/current-status", "status.xml")
+    describe "#current_status" do
+      it "should retrieve the authenticated user's current status" do
+        stub_get("/v1/people/~/current-status", "status.xml")
 
-      client.current_status.should == "New blog post: What makes a good API wrapper? http://wynnnetherland.com/2009/11/what-makes-a-good-api-wrapper/"
+        client.current_status.should == "New blog post: What makes a good API wrapper? http://wynnnetherland.com/2009/11/what-makes-a-good-api-wrapper/"
+      end
     end
 
-    it "should retrieve status updates for the authenticated user's network" do
-      stub_get("/v1/people/~/network?type=STAT", "network_statuses.xml")
+    describe "#network_statuses" do
+      it "should retrieve status updates for the authenticated user's network" do
+        stub_get("/v1/people/~/network?type=STAT", "network_statuses.xml")
 
-      stats = client.network_statuses
-      stats.updates.first.timestamp.should == 1259179809524
-      stats.updates.first.should be_is_commentable
-      stats.updates.first.profile.id.should == "19408512"
-      stats.updates.first.profile.first_name.should == 'Vahid'
-      stats.updates.first.profile.connections.first.id.should == "28072758"
-      stats.updates.first.profile.connections.first.last_name.should == 'Varone'
-      stats.updates.first.likes.size.should == 2
-      stats.updates.first.likes.last.profile.first_name.should == 'Napoleon'
-      stats.updates.last.likes.should be_empty
+        stats = client.network_statuses
+        stats.updates.first.timestamp.should == 1259179809524
+        stats.updates.first.should be_is_commentable
+        stats.updates.first.profile.id.should == "19408512"
+        stats.updates.first.profile.first_name.should == 'Vahid'
+        stats.updates.first.profile.connections.first.id.should == "28072758"
+        stats.updates.first.profile.connections.first.last_name.should == 'Varone'
+        stats.updates.first.likes.size.should == 2
+        stats.updates.first.likes.last.profile.first_name.should == 'Napoleon'
+        stats.updates.last.likes.should be_empty
+      end
     end
 
-    it "should retrieve network updates" do
-      stub_get("/v1/people/~/network?type=PICT", "picture_updates.xml")
+    describe "#network_updates" do
+      it "should retrieve network updates" do
+        stub_get("/v1/people/~/network?type=PICT", "picture_updates.xml")
 
-      stats = client.network_updates(:type => "PICT")
-      stats.updates.size.should == 4
-      stats.updates.last.profile.headline.should == "Creative Director for Intridea"
+        stats = client.network_updates(:type => "PICT")
+        stats.updates.size.should == 4
+        stats.updates.last.profile.headline.should == "Creative Director for Intridea"
+      end
     end
 
-    it "should send a message to recipients" do
-      stub_post("/v1/people/~/mailbox", "mailbox_items.xml")
+    describe "#send_message" do
+      it "should send a message to recipients" do
+        stub_post("/v1/people/~/mailbox", "mailbox_items.xml")
 
-      recipients = ["~", "abcdefg"]
-      subject    = "Congratulations on your new position."
-      body       = "You're certainly the best person for the job!"
+        recipients = ["~", "abcdefg"]
+        subject    = "Congratulations on your new position."
+        body       = "You're certainly the best person for the job!"
 
-      client.send_message(subject, body, recipients).should == "201"
+        client.send_message(subject, body, recipients).should == "201"
 
-      expect_post("/v1/people/~/mailbox", "mailbox_items.xml")
+        expect_post("/v1/people/~/mailbox", "mailbox_items.xml")
+      end
     end
 
-    it "should share a link" do
-      stub_post("/v1/people/~/shares", "blank.xml")
+    describe "#share" do
+      it "should share a link" do
+        stub_post("/v1/people/~/shares", "blank.xml")
 
-      client.share({
-        :comment => "Testing out the LinkedIn API",
-        :title => "Share",
-        :url => "http://www.linkedin.com",
-        :image_url => "http://www.linkedin.com/pretty_logo.jpg"
-      }).code.should == "201"
+        client.share({
+          :comment => "Testing out the LinkedIn API",
+          :title => "Share",
+          :url => "http://www.linkedin.com",
+          :image_url => "http://www.linkedin.com/pretty_logo.jpg"
+        }).code.should == "201"
 
-      expect_post("/v1/people/~/shares", "shares.xml")
+        expect_post("/v1/people/~/shares", "shares.xml")
+      end
     end
 
-    it "should retrieve language information from a profile" do
-      stub_get("/v1/people/~", "profile_full.xml")
+    describe "#likes" do
+      it "should retrieve likes for a network update" do
+        stub_get("/v1/people/~/network/updates/key=gNma67_AdI/likes","likes.xml")
 
-      language = client.profile.languages.last
-      language.name.should == "Klingon"
-      language.id.to_i.should == 72
+        likes = client.likes("gNma67_AdI")
+        likes.size.should == 2
+        likes.first.profile.first_name.should == "George"
+      end
     end
 
-    it "should retrieve skills from a profile" do
-      stub_get("/v1/people/~", "profile_full.xml")
+    describe "#like" do
+      it "should put a like to a network update" do
+        stub_put("/v1/people/~/network/updates/key=gNma67_AdI/is-liked","blank.xml", 201)
 
-      skill = client.profile.skills.last
-      skill.name.should == "Union negotiations"
-      skill.id.to_i.should == 38
-    end
-
-    it "should retrieve phone_number from a profile" do
-      stub_get("/v1/people/~", "profile_full.xml")
-
-      pn = client.profile.phone_numbers.last
-
-      pn.phone_type.should == "mobile"
-      pn.phone_number.should == "+444444444444"
-    end
-
-    it "should retrieve publications from a profile" do
-      stub_get("/v1/people/~", "profile_full.xml")
-
-      publication = client.profile.publications.last
-
-      publication.id.to_i.should == 31
-      publication.title.should == "How to host an awesome podcast"
-      publication.date.should == Date.civil(y=2006,m=8,d=1)
-    end
-
-    it "should retrieve patents from a profile" do
-      stub_get("/v1/people/~", "profile_full.xml")
-
-      patent = client.profile.patents.last
-
-      patent.id.to_i.should == 51
-      patent.title.should == "Time machine"
-      patent.date.should == Date.civil(y=2008,m=7,d=23)
-    end
-
-    it "should retrieve likes for a network update" do
-      stub_get("/v1/people/~/network/updates/key=gNma67_AdI/likes","likes.xml")
-
-      likes = client.likes("gNma67_AdI")
-      likes.size.should == 2
-      likes.first.profile.first_name.should == "George"
-    end
-
-    it "should put a like to a network update" do
-      stub_put("/v1/people/~/network/updates/key=gNma67_AdI/is-liked","blank.xml", 201)
-
-      result = client.like("gNma67_AdI")
-      result.code.should == "201"
+        result = client.like("gNma67_AdI")
+        result.code.should == "201"
+      end
     end
 
   end
