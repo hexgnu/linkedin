f9b953c705cdb92fd6b9c65cec4a52d6ceb1df8a
Mike Sukmanowsky <mike.sukmanowsky@oddinteractive.com>
2011-03-26 14:59:21 -0400

    Rewrote default methods and removed those that aren't shared by all update types (person, likes)

--- lib/linked_in/update.rb
+++ lib/linked_in/update.rb
@@ -1,23 +1,17 @@
 module LinkedIn
   class Update < LinkedIn::Base
-
-    %w[update_key update_type is_commentable?].each do |f|
-      define_method(f.to_sym) do
-        @doc.xpath("./update/#{f.gsub(/_/,'-').gsub(/\?$/,"")}").text
-      end
+    
+    def update_type
+      @doc.xpath('.update/update-type').text
+    end
+    
+    def is_commentable?
+      @doc.xpath("./update/is-commentable").text == "true"
     end
 
     def timestamp
       @doc.xpath('./update/timestamp').text.to_i
     end
-
-    def profile
-      Profile.new(Nokogiri::XML(@doc.xpath('./update/update-content/person').to_xml))
-    end
-
-    def likes
-      Likes.new(Nokogiri::XML(@doc.xpath('./update/likes').to_xml)).likes
-    end
-
+    
   end
 end
