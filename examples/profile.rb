49757b437262a18f14b91592c6ce4d3bb9b88ed1
Ryan Resella <ryan@codeforamerica.org>
2011-08-26 19:04:10 -0700

    Added how to pull positions and educations in examples

--- examples/profile.rb
+++ examples/profile.rb
@@ -5,6 +5,18 @@
 # get the profile for the authenticated user
 client.profile
 
+#get the profile Job History
+LinkedIn.default_profile_fields = ['positions']
+client.profile.positions
+
+#get the profile education
+LinkedIn.default_profile_fields = ['educations']
+client.profile.educations
+
+#or group them together
+LinkedIn.default_profile_fields = ['educations', 'positions']
+client.profile.positions
+
 # get a profile for someone found in network via ID
 client.profile(:id => 'gNma67_AdI')
 
