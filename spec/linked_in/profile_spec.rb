c2a531da459b0db7a370ac3853a64ba69f79bf5d
Ryan Resella <ryan@codeforamerica.org>
2011-10-07 16:19:08 -0700

    Wrote initial specs for Profile

--- spec/linked_in/profile_spec.rb
+++ spec/linked_in/profile_spec.rb
@@ -2,4 +2,17 @@ require 'helper'
 
 describe LinkedIn::Client::Profile do
 
+  before do
+    @client = LinkedIn::Client.new
+  end
+
+  describe ".profile" do
+    it "should return the basic profile for the current user" do
+      stub_get("https://api.linkedin.com/v1/people/~").
+        to_return(:body => fixture("basic.json"))
+      profile = @client.profile
+      profile.firstName.should == "Ryan"
+    end
+  end
+
 end
