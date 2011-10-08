e5eb2e74e0aa93337f4bfd981f4bce2275a72c5a
Ryan Resella <ryan@codeforamerica.org>
2011-10-08 14:30:34 -0700

    Added person path method

--- lib/linked_in/client/profile.rb
+++ lib/linked_in/client/profile.rb
@@ -3,8 +3,20 @@ module LinkedIn
     module Profile
 
       def profile(options={})
-        response = get('/v1/people/~', options)
+        response = get(person_path(options), options)
       end
+
+      private
+        def person_path(options)
+          path = "people/"
+          if options[:id]
+            path += "id=#{options[:id]}"
+          elsif options[:url]
+            path += "url=#{CGI.escape(options[:url])}"
+          else
+            path += "~"
+          end
+        end
     end
   end
 end
