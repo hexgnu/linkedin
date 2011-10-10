8c7762818feaa39ae5a82f38a79d7f9aa912dcbf
Ryan Resella <ryan@codeforamerica.org>
2011-10-10 14:55:57 -0700

    Added groups to client

--- lib/linked_in/client.rb
+++ lib/linked_in/client.rb
@@ -1,10 +1,12 @@
 module LinkedIn
   class Client < API
 
+    require 'linked_in/client/group'
     require 'linked_in/client/profile'
 
     alias :api_endpoint :endpoint
 
+    include LinkedIn::Client::Group
     include LinkedIn::Client::Profile
 
   end
