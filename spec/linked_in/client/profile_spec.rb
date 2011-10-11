b902595190b9f08327eff74583da14b030a9fff2
Ryan Resella <ryan@codeforamerica.org>
2011-10-11 14:18:39 -0700

    Created spec for specific profile fields

--- spec/linked_in/client/profile_spec.rb
+++ spec/linked_in/client/profile_spec.rb
@@ -13,6 +13,23 @@ describe LinkedIn::Client::Profile do
       profile = @client.profile
       profile.firstName.should == "Ryan"
     end
+
+    it "should return the users educations when passing a field" do
+      stub_get("/people/~:(educations)").
+        to_return(:body => fixture("full_profile.json"))
+      profile = @client.profile(:fields => ['educations'])
+      profile.educations._total.should == 2
+    end
+
+    it "should return the users education and positions when passing multiple fields" do
+      stub_get("/people/~:(educations,positions)").
+        to_return(:body => fixture("full_profile.json"))
+      profile = @client.profile(:fields => ['educations','positions'])
+      profile.educations._total.should == 2
+      profile.positions._total.should == 2
+    end
+
+
   end
 
   describe ".connections" do
