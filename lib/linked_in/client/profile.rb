a9ddd61c2189d5df89b20af411b7663dd6156966
Ryan Resella <ryan@codeforamerica.org>
2011-10-11 16:24:42 -0700

    Documented Profile module

--- lib/linked_in/client/profile.rb
+++ lib/linked_in/client/profile.rb
@@ -2,15 +2,38 @@ module LinkedIn
   class Client
     module Profile
 
-     def profile(options={})
+      # Returns the member's LinkedIn profile.
+      #
+      # @see https://developer.linkedin.com/documents/profile-api
+      # @param options [Hash] A customizable set of options.
+      # @option options fields [Hash] A list of profile fields
+      # @return [Hashie::Rash]
+      # @example
+      #   client.profile
+      #   client.profile(:fields => ['educations'])
+      #   client.profile(:fields => ['educations','positions'])
+      #   client.profile(:id => "123")
+      def profile(options={})
         path = person_path(options)
         simple_query(path, options)
       end
 
+      # Returns the list of connections for a user who has granted access to his/her account
+      #
+      # @see https://developer.linkedin.com/documents/connections-api
+      # @param options [Hash] A customizable set of options.
+      # @option options start [String] Starting location within the result set for paginated returns. Ranges are specified with a starting index and a number of results (count) to return. The default value for this parameter is 0.
+      # @option options count [String] Ranges are specified with a starting index and a number of results to return. You may specify any number. Default and max page size is 500. Implement pagination to retrieve more than 500 connections.
+      # @option options modified [String] Values are updated or new.
+      # @option options modified-since [String] Value as a Unix time stamp of milliseconds since epoch.
+      # @return [Hashie::Rash]
+      # @example
+      #   client.connections
       def connections(options={})
         path = "#{person_path(options)}/connections"
         simple_query(path, options)
       end
+
 
       def network_updates(options={})
         path = "#{person_path(options)}/network/updates"
