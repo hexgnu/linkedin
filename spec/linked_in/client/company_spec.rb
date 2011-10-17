8b6ccf669a0487675059c78bd6387a0ba81cfb00
Ryan Resella <ryan@codeforamerica.org>
2011-10-17 16:17:43 -0700

    Added examples for ID and universal name for company spec

--- spec/linked_in/client/company_spec.rb
+++ spec/linked_in/client/company_spec.rb
@@ -10,8 +10,23 @@ describe LinkedIn::Client::Company do
     it "should return the company of the id" do
       stub_get("/companies/660862").
         to_return(:body => fixture("company.json"))
-      company = @client.company(660862)
-      company.name == "Code for America"
+      company = @client.company(:id => 660862)
+      company.name.should == "Code for America"
     end
+
+    it "should return the company when searching by universal name" do
+      stub_get("/companies/universal-name=linkedin").
+        to_return(:body => fixture("company_linked_in.json"))
+      company = @client.company(:universal_name => "linkedin")
+      company.name.should == "LinkedIn"
+    end
+
+    it "should return the company filtered by email domain" do
+      stub_get("/companies?email-domain=apple.com").
+        to_return(:body => fixture("company_apple.json"))
+      company = @client.company(:email_domain => "apple.com")
+      company._total.should == 2
+    end
+
   end
 end
