68f326d73cea875c89ff7dcba8b48a5d5cbb8fbf
Ryan Resella <ryan@codeforamerica.org>
2011-10-17 16:19:16 -0700

    Implemented company by ID and universal name

--- lib/linked_in/client/company.rb
+++ lib/linked_in/client/company.rb
@@ -2,15 +2,24 @@ module LinkedIn
   class Client
     module Company
 
-     def company(id,options={})
-        path = company_path(id,options)
-        get(path, options)
+     def company(options={})
+        path = company_path(options)
+        get(path)
       end
 
       private
 
-        def company_path(id,options)
-          path = "companies/#{id}"
+        def company_path(options)
+          path = "companies"
+          if options[:id]
+            path += "/#{options[:id]}"
+          elsif options[:universal_name]
+            path += "/universal-name=#{options[:universal_name]}"
+          elsif options[:email_domain]
+            path += "?email-domain=#{options[:email_domain]}"
+          else
+            path
+          end
         end
     end
   end
