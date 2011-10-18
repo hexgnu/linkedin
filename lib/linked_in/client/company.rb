e4e6a639c8ef73bd8b11582d49dab09b5b76580f
Ryan Resella <ryan@codeforamerica.org>
2011-10-18 12:07:57 -0700

    Documented company

--- lib/linked_in/client/company.rb
+++ lib/linked_in/client/company.rb
@@ -2,10 +2,24 @@ module LinkedIn
   class Client
     module Company
 
-     def company(options={})
+      # Returns the Company Profile based on ID, Universal Name, or Email domain.
+      #
+      # @see https://developer.linkedin.com/documents/company-lookup-api-and-fields
+      # @param options [Hash] A customizable set of options.
+      # @option options id [Hash] Retreive a company using the Company ID
+      # @option options universal_name [Hash] Retreive a company using the Universal Name
+      # @option options email_domain [Hash] Retreive an array of companies that match the email domain
+      # @option options fields [Hash] A list of company fields
+      # @return [Hashie::Rash]
+      # @example
+      #   client.company(:id => 660862)
+      #   client.company(:universal_name => "linkedin")
+      #   client.company(:email_domain => "apple.com")
+      #   client.company(:id => "660862",:fields => ['id','name','ticker','description'])
+      def company(options={})
         path = company_path(options)
         simple_query(path, options)
-     end
+      end
 
       private
 
