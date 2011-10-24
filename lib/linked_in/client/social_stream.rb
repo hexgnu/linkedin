702ba9f40b746ea678b45559ddc162b1f0ba597c
Ryan Resella <ryan@codeforamerica.org>
2011-10-24 14:04:36 -0700

    Wrote documentation for social stream

--- lib/linked_in/client/social_stream.rb
+++ lib/linked_in/client/social_stream.rb
@@ -2,16 +2,37 @@ module LinkedIn
   class Client
     module SocialStream
 
+      # Returns the current share for the current user.
+      #
+      # @see https://developer.linkedin.com/documents/share-apis
+      # @param options [Hash] A customizable set of options.
+      # @return [Hashie::Rash]
+      # @example
+      #   client.current_share
       def current_share(options={})
         path = "#{person_path(options)}:(current-share)"
         get(path,options)
       end
 
+      # Return information about the member's network.
+      #
+      # @see https://developer.linkedin.com/documents/get-network-updates-and-statistics-api
+      # @param options [Hash] A customizable set of options.
+      # @return [Hashie::Rash]
+      # @example
+      #   client.network_stats
       def network_stats(options={})
         path = "#{person_path(options)}/network/network-stats"
         get(path,options)
       end
 
+      # Returns a feed of network events.
+      #
+      # @see https://developer.linkedin.com/documents/get-network-updates-and-statistics-api
+      # @param options [Hash] A customizable set of options.
+      # @return [Hashie::Rash]
+      # @example
+      #   client.network_updates
       def network_updates(options={})
         path = "#{person_path(options)}/network/updates"
         simple_query(path, options)
