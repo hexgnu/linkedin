c326e8fd1448cc0bd37d815fb070f58f6b4fbac1
Ryan Resella <ryan@codeforamerica.org>
2011-11-10 10:34:33 -0800

    Bump to 0.3.6

--- lib/linked_in/version.rb
+++ lib/linked_in/version.rb
@@ -3,7 +3,7 @@ module LinkedIn
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 3
-    PATCH = 5
+    PATCH = 6
     PRE   = nil
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
   end
