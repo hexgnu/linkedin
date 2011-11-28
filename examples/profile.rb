f00eb7ee4ffa5af42484c704d0c420624f2cb658
Dmitri Skjorshammer <dmitriskj@gmail.com>
2011-11-27 18:55:53 -0800

    Closes linkedin-84 ability to access user companies

--- examples/profile.rb
+++ examples/profile.rb
@@ -11,4 +11,8 @@ client.profile(:id => 'gNma67_AdI')
 # get a profile for someone via their public profile url
 client.profile(:url => 'http://www.linkedin.com/in/netherland')
 
+# provides the ability to access authenticated user's company field in the profile
+user = client.profile(:fields => %w(positions))
+companies = user.positions.all.map{|t| t.company}
+# Example: most recent company can be accessed via companies[0]
 
