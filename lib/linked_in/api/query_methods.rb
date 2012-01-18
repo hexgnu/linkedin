8748651416961dedaeece6e65f1aa5070d1259c1
Prashant Nadarajan <prashant.nadarajan@gmail.com>
2012-01-18 15:22:17 +0800

    support custom parameters in simple_query

--- lib/linked_in/api/query_methods.rb
+++ lib/linked_in/api/query_methods.rb
@@ -33,6 +33,7 @@ module LinkedIn
           elsif fields
             path +=":(#{fields.map{ |f| f.to_s.gsub("_","-") }.join(',')})"
           end
+          path = to_uri(path, options[:parameters])
           headers = options[:headers] || {}
           Mash.from_json(get(path, headers))
         end
