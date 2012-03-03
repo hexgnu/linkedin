c73dbe58dbd89b9d649c60b2e157be469a841181
Leonardo Rodriguez <lcrodriguez@gmail.com>
2012-03-02 21:26:54 -0300

    Fixed bug

--- lib/linked_in/helpers/authorization.rb
+++ lib/linked_in/helpers/authorization.rb
@@ -5,7 +5,7 @@ module LinkedIn
       DEFAULT_OAUTH_OPTIONS = {
         :request_token_path => "/uas/oauth/requestToken",
         :access_token_path  => "/uas/oauth/accessToken",
-        :authorize_path     => "/uas/oauth/authenticate"
+        :authorize_path     => "/uas/oauth/authenticate",
         :api_host           => "https://api.linkedin.com",
         :auth_host          => "https://www.linkedin.com"
       }
