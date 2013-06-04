d288cc455fd44157ae55dca850aa5220aa8f0eb2
Matt Kirk <meteor.kirk@gmail.com>
2013-06-03 20:34:42 -0700

    Fixing e-mail domain filtering issue brought up in PR #101

--- lib/linked_in/api/query_methods.rb
+++ lib/linked_in/api/query_methods.rb
@@ -73,17 +73,18 @@ module LinkedIn
         end
 
         def company_path(options)
-          path = "/companies/"
-          if id = options.delete(:id)
-            path += "id=#{id}"
+          path = "/companies"
+          
+          if domain = options.delete(:domain)
+            path += "?email-domain=#{CGI.escape(domain)}"
+          elsif id = options.delete(:id)
+            path += "/id=#{id}"
           elsif url = options.delete(:url)
-            path += "url=#{CGI.escape(url)}"
+            path += "/url=#{CGI.escape(url)}"
           elsif name = options.delete(:name)
-            path += "universal-name=#{CGI.escape(name)}"
-          elsif domain = options.delete(:domain)
-            path += "email-domain=#{CGI.escape(domain)}"
+            path += "/universal-name=#{CGI.escape(name)}"
           else
-            path += "~"
+            path += "/~"
           end
         end
 
