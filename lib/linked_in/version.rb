f8d3315f19ad1afea68ad774b8d0c6c8b2119530
Matt Kirk <meteor.kirk@gmail.com>
2013-06-06 12:08:49 -0700

    Bumping from 0.4.0 to 0.4.1:
    * Fixing E-mail filtering issue brought up in PR#101
    * Sanitizing query parameter building
    * Updated examples
    * Implemented Job API

--- lib/linked_in/version.rb
+++ lib/linked_in/version.rb
@@ -3,7 +3,7 @@ module LinkedIn
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 4
-    PATCH = 0
+    PATCH = 1
     PRE   = nil
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
   end
