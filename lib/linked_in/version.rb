395540d029156f29d0e53f588feb31f279aa5d70
Evan Morikawa <evan@evanmorikawa.com>
2013-07-16 13:54:12 -0400

    Bumped patch number

--- lib/linked_in/version.rb
+++ lib/linked_in/version.rb
@@ -3,7 +3,7 @@ module LinkedIn
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 1
-    PATCH = 0
+    PATCH = 1
     PRE   = nil
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
   end
