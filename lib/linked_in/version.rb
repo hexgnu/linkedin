4719b64a6320003d10a3a0857c2eba2c216ed529
Matt Kirk <meteor.kirk@gmail.com>
2013-08-06 08:35:09 -0700

    Version bump to 0.4.2 to fix issue with updates on networks

--- lib/linked_in/version.rb
+++ lib/linked_in/version.rb
@@ -3,7 +3,7 @@ module LinkedIn
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 4
-    PATCH = 1
+    PATCH = 2
     PRE   = nil
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
   end
