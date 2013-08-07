f484959485ac2ce06d04eed26727b29e9d4cb01c
Dmitriy Karpus <dmitriy.karpus@gmail.com>
2013-08-07 17:11:43 +0300

    Add tests for view picture urls

--- spec/cases/api_spec.rb
+++ spec/cases/api_spec.rb
@@ -19,6 +19,11 @@ describe LinkedIn::Api do
     stub_request(:get, "https://api.linkedin.com/v1/people/id=123").to_return(:body => "{}")
     client.profile(:id => 123).should be_an_instance_of(LinkedIn::Mash)
   end
+  
+  it "should be able to view the picture urls" do
+    stub_request(:get, "https://api.linkedin.com/v1/people/~/picture-urls::(original)").to_return(:body => "{}")
+    client.picture_urls.should be_an_instance_of(LinkedIn::Mash)
+  end
 
   it "should be able to view connections" do
     stub_request(:get, "https://api.linkedin.com/v1/people/~/connections").to_return(:body => "{}")
