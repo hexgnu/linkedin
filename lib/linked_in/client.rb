9b9dfa7facb9eda629c17d49aa7f36a7295ab6de
Matt Kirk <meteor.kirk@gmail.com>
2013-11-15 12:05:28 -0800

    Remove authorization helper and squash namespaces a bit

--- lib/linked_in/client.rb
+++ lib/linked_in/client.rb
@@ -1,46 +1,76 @@
 require 'cgi'
 
-module LinkedIn
+class LinkedIn::Client
+  include LinkedIn::Helpers::Request
+  include LinkedIn::Api::QueryMethods
+  include LinkedIn::Api::UpdateMethods
+  include LinkedIn::Search
 
-  class Client
-    include Helpers::Request
-    include Helpers::Authorization
-    include Api::QueryMethods
-    include Api::UpdateMethods
-    include Search
+  DEFAULT_OAUTH_OPTIONS = {
+    :request_token_path => "/uas/oauth/requestToken",
+    :access_token_path  => "/uas/oauth/accessToken",
+    :authorize_path     => "/uas/oauth/authorize",
+    :api_host           => "https://api.linkedin.com",
+    :auth_host          => "https://www.linkedin.com"
+  }
 
-    attr_reader :consumer_token, :consumer_secret, :consumer_options
+  attr_reader :consumer_token, :consumer_secret, :consumer_options
 
-    def initialize(ctoken=LinkedIn.token, csecret=LinkedIn.secret, options={})
-      @consumer_token   = ctoken
-      @consumer_secret  = csecret
-      @consumer_options = options
-    end
-
-    #
-    # def current_status
-    #   path = "/people/~/current-status"
-    #   Crack::XML.parse(get(path))['current_status']
-    # end
-    #
-    # def network_statuses(options={})
-    #   options[:type] = 'STAT'
-    #   network_updates(options)
-    # end
-    #
-    # def network_updates(options={})
-    #   path = "/people/~/network"
-    #   Network.from_xml(get(to_uri(path, options)))
-    # end
-    #
-    # # helpful in making authenticated calls and writing the
-    # # raw xml to a fixture file
-    # def write_fixture(path, filename)
-    #   file = File.new("test/fixtures/#{filename}", "w")
-    #   file.puts(access_token.get(path).body)
-    #   file.close
-    # end
-
+  def initialize(ctoken = LinkedIn.token, csecret = LinkedIn.secret, options = {})
+    @consumer_token   = ctoken
+    @consumer_secret  = csecret
+    @consumer_options = options
   end
 
+  def consumer
+    @consumer ||= ::OAuth::Consumer.new(@consumer_token, @consumer_secret, parse_oauth_options)
+  end
+
+  # Note: If using oauth with a web app, be sure to provide :oauth_callback.
+  # Options:
+  #   :oauth_callback => String, url that LinkedIn should redirect to
+  def request_token(options={}, *arguments, &block)
+    @request_token ||= consumer.get_request_token(options, *arguments, &block)
+  end
+
+  # For web apps use params[:oauth_verifier], for desktop apps,
+  # use the verifier is the pin that LinkedIn gives users.
+  def authorize_from_request(request_token, request_secret, verifier_or_pin)
+    request_token = ::OAuth::RequestToken.new(consumer, request_token, request_secret)
+    access_token  = request_token.get_access_token(:oauth_verifier => verifier_or_pin)
+    @auth_token, @auth_secret = access_token.token, access_token.secret
+  end
+
+  def access_token
+    @access_token ||= ::OAuth::AccessToken.new(consumer, @auth_token, @auth_secret)
+  end
+
+  def authorize_from_access(atoken, asecret)
+    @auth_token, @auth_secret = atoken, asecret
+  end
+
+  private
+
+  # since LinkedIn uses api.linkedin.com for request and access token exchanges,
+  # but www.linkedin.com for authorize/authenticate, we have to take care
+  # of the url creation ourselves.
+  def parse_oauth_options
+    {
+      :request_token_url => full_oauth_url_for(:request_token, :api_host),
+      :access_token_url  => full_oauth_url_for(:access_token,  :api_host),
+      :authorize_url     => full_oauth_url_for(:authorize,     :auth_host),
+      :site              => @consumer_options[:site] || @consumer_options[:api_host] || DEFAULT_OAUTH_OPTIONS[:api_host],
+      :proxy             => @consumer_options.fetch(:proxy, nil)
+    }
+  end
+
+  def full_oauth_url_for(url_type, host_type)
+    if @consumer_options["#{url_type}_url".to_sym]
+      @consumer_options["#{url_type}_url".to_sym]
+    else
+      host = @consumer_options[:site] || @consumer_options[host_type] || DEFAULT_OAUTH_OPTIONS[host_type]
+      path = @consumer_options[:"#{url_type}_path".to_sym] || DEFAULT_OAUTH_OPTIONS["#{url_type}_path".to_sym]
+      "#{host}#{path}"
+    end
+  end
 end
--- lib/linked_in/helpers/request.rb
+++ lib/linked_in/helpers/request.rb
@@ -1,85 +1,77 @@
-module LinkedIn
-  module Helpers
+module LinkedIn::Helpers::Request
+  DEFAULT_HEADERS = {
+  'x-li-format' => 'json'
+  }
 
-    module Request
+  API_PATH = '/v1'
 
-      DEFAULT_HEADERS = {
-        'x-li-format' => 'json'
-      }
+  protected
 
-      API_PATH = '/v1'
+  def get(path, options={})
+    response = access_token.get("#{API_PATH}#{path}", DEFAULT_HEADERS.merge(options))
+    raise_errors(response)
+    response.body
+  end
 
-      protected
+  def post(path, body='', options={})
+    response = access_token.post("#{API_PATH}#{path}", body, DEFAULT_HEADERS.merge(options))
+    raise_errors(response)
+    response
+  end
 
-        def get(path, options={})
-          response = access_token.get("#{API_PATH}#{path}", DEFAULT_HEADERS.merge(options))
-          raise_errors(response)
-          response.body
-        end
+  def put(path, body, options={})
+    response = access_token.put("#{API_PATH}#{path}", body, DEFAULT_HEADERS.merge(options))
+    raise_errors(response)
+    response
+  end
 
-        def post(path, body='', options={})
-          response = access_token.post("#{API_PATH}#{path}", body, DEFAULT_HEADERS.merge(options))
-          raise_errors(response)
-          response
-        end
+  def delete(path, options={})
+    response = access_token.delete("#{API_PATH}#{path}", DEFAULT_HEADERS.merge(options))
+    raise_errors(response)
+    response
+  end
 
-        def put(path, body, options={})
-          response = access_token.put("#{API_PATH}#{path}", body, DEFAULT_HEADERS.merge(options))
-          raise_errors(response)
-          response
-        end
+  private
 
-        def delete(path, options={})
-          response = access_token.delete("#{API_PATH}#{path}", DEFAULT_HEADERS.merge(options))
-          raise_errors(response)
-          response
-        end
-
-      private
-
-        def raise_errors(response)
-          # Even if the json answer contains the HTTP status code, LinkedIn also sets this code
-          # in the HTTP answer (thankfully).
-          case response.code.to_i
-          when 401
-            data = Mash.from_json(response.body)
-            raise LinkedIn::Errors::UnauthorizedError.new(data), "(#{data.status}): #{data.message}"
-          when 400
-            data = Mash.from_json(response.body)
-            raise LinkedIn::Errors::GeneralError.new(data), "(#{data.status}): #{data.message}"
-          when 403
-            data = Mash.from_json(response.body)
-            raise LinkedIn::Errors::AccessDeniedError.new(data), "(#{data.status}): #{data.message}"
-          when 404
-            raise LinkedIn::Errors::NotFoundError, "(#{response.code}): #{response.message}"
-          when 500
-            raise LinkedIn::Errors::InformLinkedInError, "LinkedIn had an internal error. Please let them know in the forum. (#{response.code}): #{response.message}"
-          when 502..503
-            raise LinkedIn::Errors::UnavailableError, "(#{response.code}): #{response.message}"
-          end
-        end
-
-
-        # Stolen from Rack::Util.build_query
-        def to_query(params)
-          params.map { |k, v|
-            if v.class == Array
-              to_query(v.map { |x| [k, x] })
-            else
-              v.nil? ? escape(k) : "#{CGI.escape(k.to_s)}=#{CGI.escape(v.to_s)}"
-            end
-          }.join("&")
-        end
-
-        def to_uri(path, options)
-          uri = URI.parse(path)
-
-          if options && options != {}
-            uri.query = to_query(options)
-          end
-          uri.to_s
-        end
+  def raise_errors(response)
+    # Even if the json answer contains the HTTP status code, LinkedIn also sets this code
+    # in the HTTP answer (thankfully).
+    case response.code.to_i
+    when 401
+      data = LinkedIn::Mash.from_json(response.body)
+      raise LinkedIn::Errors::UnauthorizedError.new(data), "(#{data.status}): #{data.message}"
+    when 400
+      data = LinkedIn::Mash.from_json(response.body)
+      raise LinkedIn::Errors::GeneralError.new(data), "(#{data.status}): #{data.message}"
+    when 403
+      data = LinkedIn::Mash.from_json(response.body)
+      raise LinkedIn::Errors::AccessDeniedError.new(data), "(#{data.status}): #{data.message}"
+    when 404
+      raise LinkedIn::Errors::NotFoundError, "(#{response.code}): #{response.message}"
+    when 500
+      raise LinkedIn::Errors::InformLinkedInError, "LinkedIn had an internal error. Please let them know in the forum. (#{response.code}): #{response.message}"
+    when 502..503
+      raise LinkedIn::Errors::UnavailableError, "(#{response.code}): #{response.message}"
     end
+  end
 
+  # Stolen from Rack::Util.build_query
+  def to_query(params)
+    params.map { |k, v|
+      if v.class == Array
+        to_query(v.map { |x| [k, x] })
+      else
+        v.nil? ? escape(k) : "#{CGI.escape(k.to_s)}=#{CGI.escape(v.to_s)}"
+      end
+    }.join("&")
+  end
+
+  def to_uri(path, options)
+    uri = URI.parse(path)
+
+    if options && options != {}
+      uri.query = to_query(options)
+    end
+    uri.to_s
   end
 end
