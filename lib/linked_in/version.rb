fad1a1d6ab3e9b02f15cc2e60d81cdbb39e1d1dc
Brian Petro <brian@angularjobs.com>
2013-12-05 11:34:08 -0500

    version

--- lib/linked_in/version.rb
+++ lib/linked_in/version.rb
@@ -3,7 +3,7 @@ module LinkedIn
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 5
-    PATCH = 1
+    PATCH = 4
     PRE   = nil
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
   end
