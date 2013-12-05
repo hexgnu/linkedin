1cb639ee62e8e8feae39bc45bac26835141d2f15
Brian Petro <brian@plexm.com>
2013-12-05 12:15:59 -0500

    prepare for pull

--- lib/linked_in/version.rb
+++ lib/linked_in/version.rb
@@ -2,8 +2,8 @@ module LinkedIn
 
   module VERSION #:nodoc:
     MAJOR = 0
-    MINOR = 5
-    PATCH = 4
+    MINOR = 4
+    PATCH = 3
     PRE   = nil
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
   end
