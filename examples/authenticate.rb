8d58d46044230f08da77bc7f52ca2994db41d846
Matt Kirk <meteor.kirk@gmail.com>
2013-12-17 15:46:01 -0800

    Making the authenticate.rb example a little clearer

--- examples/authenticate.rb
+++ examples/authenticate.rb
@@ -3,12 +3,17 @@ require 'linkedin'
 
 # get your api keys at https://www.linkedin.com/secure/developer
 client = LinkedIn::Client.new('your_consumer_key', 'your_consumer_secret')
-rtoken = client.request_token.token
-rsecret = client.request_token.secret
+
+# If you want to use one of the scopes from linkedin you have to pass it in at this point
+# You can learn more about it here: http://developer.linkedin.com/documents/authentication
+request_token = client.request_token({}, :scope => "r_basicprofile+r_emailaddress")
+
+rtoken = request_token.token
+rsecret = request_token.secret
 
 # to test from your desktop, open the following url in your browser
 # and record the pin it gives you
-client.request_token.authorize_url
+request_token.authorize_url
 => "https://api.linkedin.com/uas/oauth/authorize?oauth_token=<generated_token>"
 
 # then fetch your access keys
