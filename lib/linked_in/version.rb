bc06a4824a38ea078ec7ab04975c70a22d91e1d6
Matt Kirk <matt@matthewkirk.com>
2014-01-11 14:29:50 -0800

    Version 0.4.5 not 0.4.4

--- lib/linked_in/version.rb
+++ lib/linked_in/version.rb
@@ -3,7 +3,7 @@ module LinkedIn
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 4
-    PATCH = 4
+    PATCH = 5
     PRE   = nil
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
   end
