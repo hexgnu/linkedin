7348501401795b9c20497a836ef07b8d41632e6d
Matt Kirk <matt@matthewkirk.com>
2014-01-30 15:59:21 -0800

    Move connection into client

--- lib/linked_in/client.rb
+++ lib/linked_in/client.rb
@@ -22,6 +22,28 @@ module LinkedIn
       @consumer_options = options
     end
 
+    def connection_options
+      faraday_options.merge({
+        :headers => {
+          'Accept' => "application/#{format}",
+          'User-Agent' => user_agent,
+          'x-li-format' => 'json'
+        },
+        :proxy => proxy,
+        :ssl => {:verify => false},
+        :url => api_endpoint
+      })
+    end
+
+    def connection
+      @connection ||= Faraday.new(connection_options) do |builder|
+        builder.use Faraday::Request::OAuth, authentication
+        builder.use Faraday::Request::UrlEncoded
+        builder.use Faraday::Response::ParseJson
+        builder.adapter(adapter)
+      end
+    end
+
   end
 
 end
